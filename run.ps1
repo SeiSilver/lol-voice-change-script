003bbdcf009b72cceb9958e799f09e8ca2d618cd
SilverCat <datpd0507@gmail.com>
2023-01-30 00:30:36 +0700

    setup

---
+++ run.ps1
@@ -0,0 +1,13 @@
+# =================================== Start Main Program ===================================
+
+Write-Output ""
+Write-Output "---------------------------------------------------------------------------"
+Write-Output "Options:"
+Write-Output "1. Change Language to Vietnamese"
+Write-Output "2. Change Language to Japanese"
+Write-Output "3. Change Language to US"
+Write-Output "4. Change Language to Other"
+Write-Output ""
+$choice = Read-Host "Enter choice here"
+
+cmd /c pause
